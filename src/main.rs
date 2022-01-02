cf21ec71aff7450ef87fe4b20ce380adb078ac1b
moreyhat <moreyhat@gmail.com>
2022-01-02 01:31:29 -0800

    Implemented health_check app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use rust_web_sample_app::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
+    run(listener)?.await
 }
