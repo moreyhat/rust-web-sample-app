507f59ec61e4df3311c56401308eb66384f6c0e9
moreyhat <moreyhat@gmail.com>
2022-01-02 01:33:19 -0800

    Format codes

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use actix_web::{web, App, HttpResponse, HttpServer};
 use actix_web::dev::Server;
+use actix_web::{web, App, HttpResponse, HttpServer};
 use std::net::TcpListener;
 
 async fn health_check() -> HttpResponse {
@@ -8,8 +8,8 @@ async fn health_check() -> HttpResponse {
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-    .listen(listener)?
-    .run();
+        .listen(listener)?
+        .run();
 
     Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -10,7 +10,7 @@ async fn health_check_works() {
         .send()
         .await
         .expect("Failed to execute request.");
-    
+
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
