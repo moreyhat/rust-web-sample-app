b0f0505cb4e967d4cb9b305d7cc8838065ccf610
moreyhat <moreyhat@gmail.com>
2022-02-14 02:14:54 -0800

    Chapter 5

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,35 @@
-use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer};
-use std::net::TcpListener;
+#![allow(clippy::toplevel_ref_arg)]
+pub mod configuration;
+pub mod routes;
+pub mod startup;
+pub mod telemetry;
 
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
+// use actix_web::dev::Server;
+// use actix_web::{web, App, HttpResponse, HttpServer};
+// use std::net::TcpListener;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+// async fn health_check() -> HttpResponse {
+//     HttpResponse::Ok().finish()
+// }
 
-    Ok(server)
-}
+// #[derive(serde::Deserialize)]
+// pub struct FormData {
+//     email: String,
+//     name: String
+// }
+
+// async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+//     HttpResponse::Ok().finish()
+// }
+
+// pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+//     let server = HttpServer::new(|| {
+//         App::new()
+//             .route("/health_check", web::get().to(health_check))
+//             .route("/subscriptions", web::post().to(subscribe))
+//     })
+//     .listen(listener)?
+//     .run();
+
+//     Ok(server)
+// }
